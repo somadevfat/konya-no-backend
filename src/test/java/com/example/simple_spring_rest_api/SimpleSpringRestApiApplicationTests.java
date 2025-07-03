8bfe517e41f6da9002375fd1eb2b5dd41f8dba38
soma-desktop-pc <somahiranodev@gmail.com>
2025-07-03 18:13:49 +0900

    add:イニシャライザーでプロジェクト作成

---
+++ src/test/java/com/example/simple_spring_rest_api/SimpleSpringRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simple_spring_rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSpringRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
